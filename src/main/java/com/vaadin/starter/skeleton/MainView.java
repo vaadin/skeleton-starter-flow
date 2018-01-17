31ea4d6f709a38164cc9c008a3fb22961010fa17
Mikael Grankvist <mikael.grankvist@vaadin.com>
2018-01-17 08:16:50 +0200

    Add lumo theme to starter

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -20,6 +20,8 @@ import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * The main view contains a button and a template element.
@@ -27,6 +29,7 @@ import com.vaadin.flow.router.Route;
 @BodySize(height = "100vh", width = "100vw")
 @HtmlImport("styles.html")
 @Route("")
+@Theme(Lumo.class)
 public class MainView extends VerticalLayout {
 
     public MainView() {
