481b26fd6ff294e0c6f1afff4d64a85c38d8db60
Pekka Hyvönen <pekka@vaadin.com>
2018-02-02 18:24:37 +0200

    Apply Best Practices and change License

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 # Skeleton Starter for Vaadin Flow
 
 This project can be used as a starting point to create your own Vaadin Flow application.
-It has the necessary dependencies and files to get you started. 
+It has the necessary dependencies and files to help you get started.
 
-Import it to the IDE of your choice as a Maven project.
+The best way to use it by via [vaadin.com/start](https://vaadin.com/start) - you can get only the necessary parts and choose the package naming you want to use.
+There is also a [getting started tutorial](https://vaadin.com/docs/v10/flow/introduction/tutorial-get-started.html) based on this project.
+
+To access it directly from github, clone the repository and import the project to the IDE of your choice as a Maven project. You need to have Java 8 installed.
 
 Run using `mvn jetty:run` and open [http://localhost:8080](http://localhost:8080) in browser.
 
-For a Flow application example, please see the [Beverage Buddy App Starter for Flow](https://github.com/vaadin/beverage-starter-flow) instead.
+For a full Vaadin Flow application example, there is the Beverage Buddy App Starter for Flow avaiable also from [vaadin.com/start](https://vaadin.com/start) page.
--- src/main/java/com/vaadin/starter/skeleton/ExampleTemplate.java
+++ src/main/java/com/vaadin/starter/skeleton/ExampleTemplate.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2000-2017 Vaadin Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.vaadin.starter.skeleton;
 
 import com.vaadin.flow.component.Tag;
@@ -25,7 +10,7 @@ import com.vaadin.starter.skeleton.ExampleTemplate.ExampleModel;
  * Simple template example.
  */
 @Tag("example-template")
-@HtmlImport("ExampleTemplate.html")
+@HtmlImport("src/example-template.html")
 public class ExampleTemplate extends PolymerTemplate<ExampleModel> {
 
     /**
@@ -41,6 +26,9 @@ public class ExampleTemplate extends PolymerTemplate<ExampleModel> {
         getModel().setValue("Not clicked");
     }
 
+    /*
+     * Allow setting the value property from outside of the class.
+     */
     public void setValue(String value) {
         getModel().setValue(value);
     }
