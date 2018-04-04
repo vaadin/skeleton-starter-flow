58bcc23d0cbbaee92f19232946ac412111245f1a
Gilberto Torrezan <gilberto@vaadin.com>
2018-04-04 15:14:53 +0300

    Removed the @Theme declaration

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -5,8 +5,6 @@ import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * The main view contains a button and a template element.
@@ -14,13 +12,13 @@ import com.vaadin.flow.theme.lumo.Lumo;
 @BodySize(height = "100vh", width = "100vw")
 @HtmlImport("styles/shared-styles.html")
 @Route("")
-@Theme(Lumo.class)
 public class MainView extends VerticalLayout {
 
     public MainView() {
         ExampleTemplate template = new ExampleTemplate();
 
-        Button button = new Button("Click me", event -> template.setValue("Clicked!"));
+        Button button = new Button("Click me",
+                event -> template.setValue("Clicked!"));
 
         add(button, template);
         setClassName("main-layout");
