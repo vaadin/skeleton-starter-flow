55dc4ca2808f7429c4aeec4dd18220dac36c2e85
Pekka Maanpää <pekkamaa@vaadin.com>
2018-04-10 13:05:12 +0300

    Remove @BodySize (#81)
    * Remove @BodySize
    
    After https://github.com/vaadin/flow/pull/3821 the full body size is applied by default.
    
    * Use maven plugin beta6

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -3,13 +3,11 @@ package com.vaadin.starter.skeleton;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
 
 /**
  * The main view contains a button and a template element.
  */
-@BodySize(height = "100vh", width = "100vw")
 @HtmlImport("styles/shared-styles.html")
 @Route("")
 public class MainView extends VerticalLayout {
