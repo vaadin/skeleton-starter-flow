b223d73a31e10b24078fbf6a262bcde612b8b5ef
Matti Tahvonen <matti@vaadin.com>
2018-08-28 10:05:24 +0300

    Simplified the skeleton starter (#102)
    * Simplified the skeleton starter as it should be simple

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -1,24 +1,19 @@
 package com.vaadin.starter.skeleton;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 /**
- * The main view contains a button and a template element.
+ * The main view contains a button and a click listener.
  */
-@HtmlImport("styles/shared-styles.html")
-@Route("")
+@Route
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        ExampleTemplate template = new ExampleTemplate();
-
         Button button = new Button("Click me",
-                event -> template.setValue("Clicked!"));
-
-        add(button, template);
-        setClassName("main-layout");
+                event -> Notification.show("Clicked!"));
+        add(button);
     }
 }
