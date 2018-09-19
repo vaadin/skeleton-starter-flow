8d21f347ac69d837b3dd1bf7e068b1b2543dafde
Mikko Salminen <mikko@vaadin.com>
2018-09-19 13:58:07 +0300

    PWA support for starter. (#106)
    - Added @PWA -annotation
    - Added icon

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -4,11 +4,13 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
 
 /**
  * The main view contains a button and a click listener.
  */
 @Route
+@PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
 public class MainView extends VerticalLayout {
 
     public MainView() {
