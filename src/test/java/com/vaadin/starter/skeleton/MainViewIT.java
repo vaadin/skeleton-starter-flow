934453e9a3a4e12e9a81f38d4cf329c708e8fd98
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2019-11-12 16:01:23 +0200

    Remove the statistic test from starter (#255)
    * Remove the statistic test from starter

--- src/test/java/com/vaadin/starter/skeleton/MainViewIT.java
+++ src/test/java/com/vaadin/starter/skeleton/MainViewIT.java
@@ -2,7 +2,6 @@ package com.vaadin.starter.skeleton;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 import com.vaadin.flow.component.button.testbench.ButtonElement;
@@ -35,27 +34,5 @@ public class MainViewIT extends AbstractViewTest {
     public void buttonIsUsingLumoTheme() {
         WebElement element = $(ButtonElement.class).first();
         assertThemePresentOnElement(element, Lumo.class);
-    }
-
-    @Test
-    public void usageStatisticIsLogged() throws InterruptedException {
-        Assert.assertTrue($(ButtonElement.class).exists());
-        // wait 5 seconds for collecting values in local storage
-        Thread.sleep(5000);
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        Object mode = js.executeScript("return Vaadin.developmentMode");
-
-        String item = (String) js.executeScript(
-                "return window.localStorage.getItem('vaadin.statistics.basket');");
-
-        if(Boolean.TRUE.equals(mode)){
-            Assert.assertTrue("Under development mode, the checked usage statistics are not found",
-                    item.contains("flow") && item.contains("java") && item.contains("vaadin-button"));
-        } else {
-            Assert.assertTrue("Under production mode, the usage statistics info should be empty",
-                    (item == null || item.length() == 0));
-        }
-
     }
 }
