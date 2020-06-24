43324cf7e1ebe185efe8bddd51d91fc6ad379e2f
Guillermo Alvarez <guillermo@vaadin.com>
2020-06-24 21:56:27 +0300

    Test org chart 14.2

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -1,12 +1,17 @@
 package org.vaadin.example;
 
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.ChartType;
+import com.vaadin.flow.component.charts.model.Configuration;
+import com.vaadin.flow.component.charts.model.DataLabels;
+import com.vaadin.flow.component.charts.model.Level;
+import com.vaadin.flow.component.charts.model.Node;
+import com.vaadin.flow.component.charts.model.NodeLayout;
+import com.vaadin.flow.component.charts.model.NodeSeries;
+import com.vaadin.flow.component.charts.model.PlotOptionsOrganization;
+import com.vaadin.flow.component.charts.model.style.SolidColor;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
@@ -20,26 +25,127 @@ import com.vaadin.flow.server.PWA;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addThemeName("bordered");
+        setSizeFull();
+        Chart chart = new Chart(ChartType.ORGANIZATION);
 
-        // Button click listeners can be defined as lambda expressions
-        GreetService greetService = new GreetService();
-        Button button = new Button("Say hello",
-                e -> Notification.show(greetService.greet(textField.getValue())));
+        Configuration conf = chart.getConfiguration();
+        conf.getChart().setInverted(true);
+        conf.getTooltip().setOutside(true);
+        conf.setTitle("Acme organization chart");
+        conf.getExporting().setEnabled(true);
 
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button is more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        NodeSeries series = createSeries();
+        conf.addSeries(series);
 
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
+        add(chart);
+        expand(chart);
     }
+
+    private NodeSeries createSeries() {
+        NodeSeries series = new NodeSeries();
+        series.setName("Acme");
+        Node acme = new Node("Acme");
+        Node headOffice = new Node("Head Office");
+        Node labs = new Node("Labs");
+        Node coyoteBuilding = new Node("Coyote Building");
+        Node roadRunnerBuilding = new Node("Road Runner Building");
+        Node sales = new Node("Sales");
+        Node marketing = new Node("Marketing");
+        Node accounting = new Node("Accounting");
+        Node administration = new Node("Administration");
+        Node mdsOffice = new Node("MD's Office");
+
+        Node josephMiler = new Node("Joseph Miler");
+        josephMiler.setTitle("Head of Sales");
+        josephMiler.setLayout(NodeLayout.HANGING);
+
+        Node erikPerez = new Node("Erik Perez");
+        erikPerez.setTitle("Head of Marketing");
+        erikPerez.setLayout(NodeLayout.HANGING);
+
+        Node emilyFox = new Node("Emily Fox");
+        emilyFox.setTitle("Head of Accounting");
+
+        Node ewanHerbert = new Node("Ewan Herbert");
+        ewanHerbert.setTitle("Head of Admin");
+        ewanHerbert.setLayout(NodeLayout.HANGING);
+
+        Node kateKirby = new Node("Kate Kirby");
+        Node vaughnWhiting = new Node("Vaughn Whiting");
+        Node lisaWarner = new Node("Lisa Warner");
+        Node mollyDodd = new Node("Molly Dodd");
+        Node natashaKelly = new Node("Natasha Kelly");
+
+        Node managingDirector = new Node("Sally Brown", "Sally Brown",
+                "Managing Director");
+        managingDirector.setColor(new SolidColor("#E4B651"));
+
+        series.add(acme, headOffice);
+        series.add(acme, labs);
+        series.add(headOffice, coyoteBuilding);
+        series.add(headOffice, roadRunnerBuilding);
+        series.add(coyoteBuilding, sales);
+        series.add(coyoteBuilding, marketing);
+        series.add(coyoteBuilding, accounting);
+        series.add(roadRunnerBuilding, administration);
+        series.add(roadRunnerBuilding, mdsOffice);
+        series.add(sales, josephMiler);
+        series.add(marketing, erikPerez);
+        series.add(accounting, emilyFox);
+        series.add(administration, ewanHerbert);
+        series.add(josephMiler, kateKirby);
+        series.add(josephMiler, vaughnWhiting);
+        series.add(erikPerez, lisaWarner);
+        series.add(ewanHerbert, mollyDodd);
+        series.add(ewanHerbert, natashaKelly);
+        series.add(mdsOffice, managingDirector);
+
+        PlotOptionsOrganization plotOptions = createPlotOptions();
+        series.setPlotOptions(plotOptions);
+
+        return series;
+    }
+
+    private PlotOptionsOrganization createPlotOptions() {
+        PlotOptionsOrganization plotOptions = new PlotOptionsOrganization();
+        plotOptions.setColorByPoint(false);
+        plotOptions.setColor(new SolidColor("#007ad0"));
+        DataLabels dataLabels = new DataLabels();
+        dataLabels.setColor(SolidColor.BLACK);
+        plotOptions.setDataLabels(dataLabels);
+        plotOptions.setBorderColor(SolidColor.WHITE);
+
+        Level level0 = new Level();
+        level0.setLevel(0);
+        level0.setColor(new SolidColor("#99AED3"));
+
+        Level level1 = new Level();
+        level1.setLevel(1);
+        level1.setColor(new SolidColor("#CCE6C3"));
+
+        Level level2 = new Level();
+        level2.setLevel(2);
+        level2.setColor(new SolidColor("#E1F39D"));
+
+        Level level3 = new Level();
+        level3.setLevel(3);
+        level3.setColor(new SolidColor("#CCE6C3"));
+
+        Level level4 = new Level();
+        level4.setLevel(4);
+        level4.setColor(new SolidColor("#CABEDC"));
+
+        Level level5 = new Level();
+        level5.setLevel(5);
+        level5.setColor(new SolidColor("#CABDD4"));
+
+        plotOptions.addLevel(level0);
+        plotOptions.addLevel(level1);
+        plotOptions.addLevel(level2);
+        plotOptions.addLevel(level3);
+        plotOptions.addLevel(level4);
+
+        return plotOptions;
+    }
+
 }
