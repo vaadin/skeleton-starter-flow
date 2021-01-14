76522476f1d2f55107ad309d6938bc028a3f8aaa
caalador <mikael.grankvist@gmail.com>
2021-01-14 15:10:56 +0200

    Change theme to custom theme. (#415)
    Part of vaadin/flow#9597

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -3,7 +3,6 @@ package org.vaadin.example;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -14,8 +13,6 @@ import com.vaadin.flow.router.Route;
  * that shows a greeting message in a notification.
  */
 @Route("")
-@CssImport("./styles/shared-styles.css")
-@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class MainView extends VerticalLayout {
 
     public MainView() {
