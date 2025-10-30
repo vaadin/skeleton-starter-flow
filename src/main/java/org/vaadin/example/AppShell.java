7f1577911e7720bb0798e31c4e3e48a5ef24f3f3
mikhail <mikhail@vaadin.com>
2025-10-30 15:40:54 +0200

    clean up and renamings

--- src/main/java/org/vaadin/example/AppShell.java
+++ src/main/java/org/vaadin/example/AppShell.java
@@ -1,19 +1,14 @@
 package org.vaadin.example;
 
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.aura.Aura;
 
 /**
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  */
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
-@StyleSheet("styles.css")
-@StyleSheet(Aura.STYLESHEET)
-//@CssImport("@vaadin/aura/aura.css") // works
 //@CssImport("styles-in-frontend.css")
 public class AppShell implements AppShellConfigurator {
 }
--- src/main/java/org/vaadin/example/LoginFormExporter.java
+++ src/main/java/org/vaadin/example/LoginFormExporter.java
@@ -2,14 +2,9 @@ package org.vaadin.example;
 
 import com.vaadin.flow.component.WebComponentExporter;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.webcomponent.WebComponent;
-import com.vaadin.flow.theme.aura.Aura;
 
-//@CssImport("@vaadin/aura/aura.css") // works
-//@CssImport("styles-in-frontend.css")
-@StyleSheet("styles.css")
-//@StyleSheet(Aura.STYLESHEET)
+@CssImport("styles-in-frontend.css")
 public class LoginFormExporter
         extends WebComponentExporter<LoginForm> {
     public LoginFormExporter() {
