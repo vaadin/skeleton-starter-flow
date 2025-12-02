9006a80ffe60b70ba6d32b75b2dd692c67d47619
Artur Signell <artur@vaadin.com>
2025-12-02 18:38:11 +0200

    chore: Use Lumo for now

--- src/main/java/org/vaadin/example/AppShell.java
+++ src/main/java/org/vaadin/example/AppShell.java
@@ -3,13 +3,14 @@ package org.vaadin.example;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  */
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
+@StyleSheet(Lumo.STYLESHEET)
 @StyleSheet("styles.css")
 public class AppShell implements AppShellConfigurator {
 }
